8c14df71541f0351254bebd0fd1827e5fc4cedfd
unknown <v82969775@gmail.com>
2024-10-05 16:08:05 +0300

    add index

--- tsk-1.js
+++ tsk-1.js
@@ -1,18 +1,12 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-
-    const totalPrice = quantity * pricePerDroid;
-
-    if (totalPrice > customerCredits) {
-        return "Insufficient funds!";
-    } else {
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function generatePassword(length) {
+    const characters =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
     }
+    return password;
 }
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 
-credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 
-credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 
-credits!"
+console.log(generatePassword(8));
+console.log(generatePassword(12));
